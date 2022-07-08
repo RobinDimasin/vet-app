d31fba40aec4fd5db335a02ad88efff246ea8bb6
RobinDimasin <robindimasin42@gmail.com>
2022-07-08 17:52:59 +0800

    fixed register and accountprovider

---
+++ pages/api/account/register/[type].js
@@ -0,0 +1,33 @@
+import EntityList from "@db/entity/EntityList";
+import STATUS from "@db/status";
+import Account from "@db/entity/Account/AccountEntity";
+import withAccount from "lib/middleware/withAccount";
+
+export const register = async (req, res, type) => {
+  if (req.method === "POST") {
+    type ??= req.query.type;
+
+    const account_details = req.body;
+
+    if (
+      !Array.from(Account.children)
+        .map((children) => children.entityType)
+        .includes(type)
+    ) {
+      return res.status(400).json({
+        status: STATUS.NOT_OK,
+        message: `Invalid Account type "${type}"`,
+      });
+    }
+
+    const response = await EntityList[type].new(account_details);
+
+    res.status(response.status === STATUS.OK ? 200 : 400).json(response);
+  } else {
+    res
+      .send(405)
+      .json({ status: STATUS.NOT_OK, message: "Method not allowed" });
+  }
+};
+
+export default withAccount(register, ["admin"]);
