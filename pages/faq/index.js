921e95ca9c9d122cfb4b1de4ac6b21b0df2d0718
yshaaang <erandiotrisha@gmail.com>
2022-07-12 15:26:25 +0800

    FAQ Page

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,3 +1,11 @@
 export default function FAQ() {
-    return <div > FAQ FAQ FAQ < /div>;
+    return <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+    <div class="collapse-title text-xl font-medium">
+      What is Vetreatment?
+    </div>
+    <div class="collapse-content"> 
+      <p>Vetreatment is chuchu</p>
+    </div>
+  </div>
+
 }
