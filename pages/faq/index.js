9d2ea0648a0796767498ffdcdead2ab95a6eb6af
RobinDimasin <robindimasin42@gmail.com>
2022-07-12 20:52:53 +0800

    Fixed attributes

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,102 +1,132 @@
 export default function FAQ() {
-    return (
-      <>
-        <div className="px-[10%] space-y-4">
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  What is Vetreatment?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        Vetreatment is a veterinary clinic for the short-term or 
-                        long-term medical care and treatment of animals.
-                      </p>
-                </div>
+  return (
+    <>
+      <div className="px-[10%] space-y-4">
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What is Vetreatment?
           </div>
-
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                What kind of services does Vetreatment offer?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        Vetreatment provides check-ups, treatments, diagnoses, vaccinations, 
-                        deworming, general health care, and animal observation.
-                      </p>
-                </div>
-          </div>
-
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  How can I book an appointment for my pet?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        [NOTE!] If you already have an account, you can continue right away; otherwise, you must first register.
-                      </p>
-                      <p>
-                      < br /> 
-                        &nbsp; 1. Go to HOME {'>'} Click "MAKE AN APPOINTMENT NOW!" button.
-                      < br /> 
-                        &nbsp; 2. My Appointments page {'>'} Click "+ MAKE AN APPOINTMENT" button.
-                      <br />
-                        &nbsp; 3. Choose date of appointment {'>'} Select your pet.
-                      <br />
-                        &nbsp; 4. Choose reason of appointment and description {'>'} Click "Submit".
-                      </p>
-                      <p>
-                      < br /> 
-                        You can now view your appointments on Appointments Page.
-                      </p>
-                </div>
-          </div>
-
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  Where are you located?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        We are located at 135 Legaspi Village, Makati City, Philippines
-                      </p>
-                </div>
-          </div>
-
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  What forms of payment do you accept?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        The cashier on our clinic will handle all payment transactions. 
-                        You can use credit card, debit card, or cash payment.
-                      </p>
-                </div>
-            </div>
-
-            <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  Do I need to register first to make an appointment?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        Yes, every client must have a registered account on our website.
-                      </p>
-                </div>
-          </div>
-
-          <div tabindex="0" id = "FAQ" class="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box">
-              <div class="collapse-title text-xl font-medium">
-                  Can I make multiple appointments?
-              </div>
-                <div class="collapse-content"> 
-                      <p>
-                        Yes, you can make multiple appointment for your pets.
-                      </p>
-                </div>
+          <div className="collapse-content">
+            <p>
+              Vetreatment is a veterinary clinic for the short-term or long-term
+              medical care and treatment of animals.
+            </p>
           </div>
         </div>
-        </>
-    );
 
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What kind of services does Vetreatment offer?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Vetreatment provides check-ups, treatments, diagnoses,
+              vaccinations, deworming, general health care, and animal
+              observation.
+            </p>
+          </div>
+        </div>
+
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            How can I book an appointment for my pet?
+          </div>
+          <div className="collapse-content">
+            <p>
+              [NOTE!] If you already have an account, you can continue right
+              away; otherwise, you must first register.
+            </p>
+            <p>
+              <br />
+              &nbsp; 1. Go to HOME {">"} Click "MAKE AN APPOINTMENT NOW!"
+              button.
+              <br />
+              &nbsp; 2. My Appointments page {">"} Click "+ MAKE AN APPOINTMENT"
+              button.
+              <br />
+              &nbsp; 3. Choose date of appointment {">"} Select your pet.
+              <br />
+              &nbsp; 4. Choose reason of appointment and description {">"} Click
+              "Submit".
+            </p>
+            <p>
+              <br />
+              You can now view your appointments on Appointments Page.
+            </p>
+          </div>
+        </div>
+
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Where are you located?
+          </div>
+          <div className="collapse-content">
+            <p>
+              We are located at 135 Legaspi Village, Makati City, Philippines
+            </p>
+          </div>
+        </div>
+
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What forms of payment do you accept?
+          </div>
+          <div className="collapse-content">
+            <p>
+              The cashier on our clinic will handle all payment transactions.
+              You can use credit card, debit card, or cash payment.
+            </p>
+          </div>
+        </div>
+
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Do I need to register first to make an appointment?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, every client must have a registered account on our website.
+            </p>
+          </div>
+        </div>
+
+        <div
+          tabIndex="0"
+          id="FAQ"
+          className="collapse collapse-plus border border-base-300 bg-primary text-base-300 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Can I make multiple appointments?
+          </div>
+          <div className="collapse-content">
+            <p>Yes, you can make multiple appointment for your pets.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
